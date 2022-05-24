8ea24f5e8cee887387c990709b2f669a9121b4ec
Jan Schmitt <git@smittie.de>
2022-05-24 23:38:57 +0200

    feat: provision tmux plugins via home-manager
    * effectively get rid of tpm_plugins

--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -6,17 +6,32 @@
     package = pkgs.tmux;
     baseIndex = 1;
     disableConfirmationPrompt = false;
-    escapeTime = 500;
+    escapeTime = 40;
     historyLimit = 5000;
     # keyMode = "vi";
     newSession = true;
-    # plugins = with pkgs; [
-    # tmuxPlugins.urlview
-    # {
-    # plugin = tmuxPlugins.resurrect;
-    # extraConfig = "set -g @resurrect-strategy-nvim 'session'";
-    # }
-    # ];
+    plugins = with pkgs; [
+      tmuxPlugins.urlview
+      tmuxPlugins.onedark-theme
+      tmuxPlugins.prefix-highlight
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-dir '~/.local/share/tmux/resurrect'
+          set -g @resurrect-capture-pane-contents 'on'
+        '';
+      }
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '10' # minutes
+          set -g @continuum-boot 'on'
+          set -g @continuum-boot-options 'iterm'
+        '';
+      }
+    ];
     prefix = "C-a";
     resizeAmount = 10;
     secureSocket = true;
