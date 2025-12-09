c891c893e470d3d7f526f0d1392ae9ead2aae102
Jan Schmitt <git@smittie.de>
2025-12-09 09:54:58 +0100

    fix(home-manager): adapt ssh config to 25.11 changes

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -3,21 +3,23 @@
 {
   home.packages = with pkgsUnstable; [ ssh-audit sshpass mosh ];
 
-  programs.ssh.matchBlocks."*" = {
+  programs.ssh = {
     enable = true;
-    compression = true;
-    controlMaster = "auto";
-    controlPath = "/tmp/ssh_mux_%h_%p_%r";
-    controlPersist = "10m";
-    forwardAgent = true;
-    hashKnownHosts = false;
-    addKeysToAgent = "yes";
-    # includes = [ "hosts" ];
+    enableDefaultConfig = false;
+
+    matchBlocks."*" = {
+      compression = true;
+      controlMaster = "auto";
+      controlPath = "/tmp/ssh_mux_%h_%p_%r";
+      controlPersist = "10m";
+      forwardAgent = true;
+      hashKnownHosts = false;
+      addKeysToAgent = "yes";
+    };
     extraConfig = ''
       # AddKeysToAgent yes
       UseKeychain yes
     '';
-
   };
 
 }
